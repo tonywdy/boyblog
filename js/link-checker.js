a714cc6485662fb8428be7b660a6bd20d0e67cd4
tonywdy <wudingyi1020@163.com>
2022-10-07 08:54:43 +0800

    Site updated: 2022-10-07 08:54:42

--- js/link-checker.js
+++ js/link-checker.js
@@ -11,7 +11,7 @@ async function checkLink(){
     for(var i=0;i<link.length;i++){
         if(link[i].href==="" || link[i].className==="gitter-open-chat-button")continue;//去除Gitter聊天框的影响
         if(!await checkLocalSite(link[i].href)){
-            link[i].href = "https://tonywdy.github.io/go-jumper/#"+window.btoa(link[i].href)
+            link[i].href = "https://go-jumper-2k8.pages.dev/#"+window.btoa(link[i].href)
             console.log("edit.")
         }
     }
@@ -20,7 +20,7 @@ async function checkLocalSite(url){
     try{
         console.log("check:",url)
         let domain = url.split("/")[2];
-        if(domain.endsWith("boyblog.pages.dev")||domain.endsWith("http://localhost/"))return true;
+        if(domain.endsWith("boyblog.pages.dev")||domain.endsWith("localhost:4000"))return true;
         return false;
     }catch(err){
         return true;
